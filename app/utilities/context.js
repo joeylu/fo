e97192d82a4a48b58396105dc994c243201fd5e3
Joey <joelu@firstgate.net>
2021-10-21 12:11:10 +0800

    clean up ignored files for expo

--- app/utilities/context.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const AppContext = React.createContext();
-
-export default AppContext;
-
-
-  //global state using context and hook, https://www.savaslabs.com/blog/using-react-global-state-hooks-and-context
